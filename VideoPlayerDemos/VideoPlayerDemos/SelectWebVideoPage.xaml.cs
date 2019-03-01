303402d05ee35fe24182c6f8e08c57c5c79be996
ZHANG Hu <zhnuaa@live.cn>
2019-03-02 03:01:37 +0800

    添加项目文件。

---
+++ VideoPlayerDemos/VideoPlayerDemos/SelectWebVideoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using FormsVideoLibrary;
+using Xamarin.Forms;
+
+namespace ClipBrowser
+{
+    public partial class SelectWebVideoPage : ContentPage
+    {
+        public SelectWebVideoPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (args.SelectedItem != null)
+            {
+                string key = ((string)args.SelectedItem).Replace(" ", "").Replace("'", "");
+                videoPlayer.Source = (UriVideoSource)Application.Current.Resources[key];
+            }
+        }
+    }
+}
